0459a950c0165ac922575afca2e6b1fcc2f51e99
KimGunWoo9595 <medneter@naver.com>
2023-01-30 16:07:21 +0900

    feat : spring security 로그인 로그아웃 추가

--- src/main/java/com/studyhere/studyhere/config/SecurityConfig.java
+++ src/main/java/com/studyhere/studyhere/config/SecurityConfig.java
@@ -1,21 +1,57 @@
 package com.studyhere.studyhere.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    private final UserDetailsService userDetailsService;
+    private final DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/","/**", "/login", "/sign-up", "/check-email-token",
+                        "/email-login", "/login-by-email", "/search/study").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+
+        //form Login 기능 사용
+        http.formLogin()
+                .loginPage("/login").permitAll();
+        //form Logout 기능 사용 로그아웃 성공시 -> "/"
+        http.logout()
+                .logoutSuccessUrl("/");
+
+
+    }
+
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+   /* @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .httpBasic().disable()
@@ -26,9 +62,6 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/", "/login", "/sign-up","/check-email","/check-email-token","/email-login","/check-email-login","/login-link").permitAll() // join, login은 언제나 가능
                 .antMatchers(HttpMethod.GET, "/profile/*").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/**").authenticated()
-                .antMatchers(HttpMethod.PUT, "/api/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/api/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -36,7 +69,7 @@ public class SecurityConfig {
                 .logout().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-              /*  .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)*/
+              *//*  .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)*//*
                 .build();
-    }
+    }*/
 }
