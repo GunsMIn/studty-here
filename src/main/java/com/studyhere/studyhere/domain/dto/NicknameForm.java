c8ae33b4ccba9b1499a41185aa754fcbc9828a07
KimGunWoo9595 <medneter@naver.com>
2023-02-01 17:04:57 +0900

    feat : 닉네임 변경

--- src/main/java/com/studyhere/studyhere/domain/dto/NicknameForm.java
+++ src/main/java/com/studyhere/studyhere/domain/dto/NicknameForm.java
@@ -1,4 +1,15 @@
 package com.studyhere.studyhere.domain.dto;
 
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
 public class NicknameForm {
+
+    @NotBlank
+    @Length(min = 3, max = 20)
+    private String nickname;
 }
--- src/main/java/com/studyhere/studyhere/validator/NicknameValidator.java
+++ src/main/java/com/studyhere/studyhere/validator/NicknameValidator.java
@@ -1,4 +1,31 @@
 package com.studyhere.studyhere.validator;
 
-public class NicknameValidator {
+
+import com.studyhere.studyhere.domain.dto.NicknameForm;
+import com.studyhere.studyhere.domain.entity.Account;
+import com.studyhere.studyhere.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+/**닉네임 변경 시 사용가능한 닉네임인지 확인하는 validator **/
+@Component
+@RequiredArgsConstructor
+public class NicknameValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return NicknameForm.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        NicknameForm nicknameForm = (NicknameForm) target;
+        Account byNickname = accountRepository.findByNickname(nicknameForm.getNickname());
+        if (byNickname != null) {
+            errors.rejectValue("nickname","wrong.value","이미 존재하는 닉네입입니다.");
+        }
+    }
 }
